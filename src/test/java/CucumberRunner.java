20d10f4eb4b2dd781931f626cbf975f1a43bbb09
Dmitry <dmitriyy.ev@gmail.com>
2021-02-17 20:58:45 +0300

    runner

--- src/test/java/CucumberRunner.java
+++ src/test/java/CucumberRunner.java
@@ -4,7 +4,9 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        plugin = {"io.qameta.allure.cucumber5jvm.AllureCucumber5Jvm"},
+        plugin = {"io.qameta.allure.cucumber5jvm.AllureCucumber5Jvm",
+        		 "progress",
+                 "summary"},
         glue = {"steps"},
         features = {"src/test/resources/"},
         tags = "@checkMortgage"
